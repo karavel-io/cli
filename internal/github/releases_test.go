665fc6e4175dfd55a36603db04bb596dc7dd822a
Matteo Joliveau <matteojoliveau@gmail.com>
2022-04-19 10:54:34 +0200

    Change how init works
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

---
+++ internal/github/releases_test.go
@@ -0,0 +1,15 @@
+package github
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSortReleases(t *testing.T) {
+	rels := []string{"2022.1", "2021.2", "2022.5-rc.1", "2022.5-rc.7", "2022.4-rc.2"}
+	exp := []string{"2022.5-rc.7", "2022.5-rc.1", "2022.4-rc.2", "2022.1", "2021.2"}
+
+	sortReleases(rels)
+	assert.Equal(t, exp, rels)
+}
