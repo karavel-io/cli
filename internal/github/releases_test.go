80723a3894a4d09edf5a20e37eecf824b64f2c13
Matteo Joliveau <matteojoliveau@gmail.com>
2022-04-19 11:07:02 +0200

    Properly handle the order of non-rc releases
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- internal/github/releases_test.go
+++ internal/github/releases_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestSortReleases(t *testing.T) {
-	rels := []string{"2022.1", "2021.2", "2022.5-rc.1", "2022.5-rc.7", "2022.4-rc.2"}
-	exp := []string{"2022.5-rc.7", "2022.5-rc.1", "2022.4-rc.2", "2022.1", "2021.2"}
+	rels := []string{"2022.1", "2021.2", "2022.5-rc.1", "2022.5", "2022.5-rc.7", "2022.4-rc.2"}
+	exp := []string{"2022.5", "2022.5-rc.7", "2022.5-rc.1", "2022.4-rc.2", "2022.1", "2021.2"}
 
 	sortReleases(rels)
 	assert.Equal(t, exp, rels)
