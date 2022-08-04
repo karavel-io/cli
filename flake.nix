fa06cef0cfe9432d556dcfe1d6ea9e207fa922b2
Matteo Joliveau <matteojoliveau@gmail.com>
2022-08-04 10:47:13 +0200

    Improve flake
    Signed-off-by: Matteo Joliveau <matteojoliveau@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,53 +1,41 @@
 {
   description = "Karavel CLI tool";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      trim = s: nixpkgs.lib.strings.removePrefix " " (nixpkgs.lib.strings.removeSuffix " " s);
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-      version = trim (builtins.readFile ./VERSION);
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-    in
-    {
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        trim = s: nixpkgs.lib.strings.removePrefix " v" (nixpkgs.lib.strings.removeSuffix " " (nixpkgs.lib.removeSuffix "\n" s));
+        lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+        version = trim (builtins.readFile ./VERSION);
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.buildGoModule {
+          pname = "karavel";
+          version = "v${version}";
+          src = ./.;
+          subPackages = [ "cmd/karavel" ];
+          ldflags = [
+            "-X github.com/karavel-io/cli/internal/version.version=${version}"
+          ];
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          karavel = pkgs.buildGoModule {
-            pname = "karavel";
-            version = "v${version}";
-            src = ./.;
-            subPackages = [ "cmd/karavel" ];
-            ldflags = [
-              "-X github.com/karavel-io/cli/internal/version.version=${version}"
-            ];
+          vendorSha256 = "sha256-8Ty26vr/J1SgY/+W4ZpMM1RpxrXesByXRTylOqgpYuc=";
+        };
 
-            vendorSha256 = "sha256-8Ty26vr/J1SgY/+W4ZpMM1RpxrXesByXRTylOqgpYuc=";
-          };
-        });
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.karavel);
-      defaultApp = forAllSystems (system: self.packages.${system}.karavel);
-      devShell = forAllSystems
-        (system:
-          let pkgs = nixpkgsFor.${system}; in
-          pkgs.mkShell {
+        apps.default = utils.lib.mkApp { drv = self.packages.${system}.default; };
+        devShells.default = pkgs.mkShell
+          {
             buildInputs = with pkgs; [
               go_1_18
               addlicense
               gnumake
               goreleaser
             ];
-          });
-    };
+          };
+      });
 }
